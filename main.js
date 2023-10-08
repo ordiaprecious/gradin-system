285298ebe4e51a9c17a1fac0eed455abcd8b7933
ordiaprecious <ordiaprecious@gmail.com>
2023-10-08 17:23:22 +0100

    second commit

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-// function f(){
-//     console.log('hvuvhkbj')
-// }
-var can=document.getElementsByTagName('input(#name)').value
+prompt('input your username to check your fucking result')
+
 function first(){
-    document.getElementById('bigscreen').innerHTML= can
+    var can=document.getElementById('name').value  
+    document.getElementById('screen').innerHTML= 'candidate: ' +can 
+    
 }
 function clear(){
-    document.getElementById('bigscreen').innerText =''
+    var empty=document.getElementById('screen').value=''
+    document.getElementById('screen').value =empty
     console.log('djd')
 }
 
@@ -19,6 +20,11 @@ function finish(){
     var cor6= document.getElementById('cod').value;
     var cor7= document.getElementById('arts').value;
     var add=parseFloat(cor1)+ parseFloat(cor2) + parseFloat(cor3) +parseFloat(cor4) +parseFloat(cor5) + parseFloat(cor6) +parseFloat(cor7)
-    document.getElementById('bigscreen').innerHTML = 'your total grade is: '+ add /7
+    goal=(add * 100) /700 +'%'
+    document.getElementById('bigscreen').innerHTML = 'your total grade is: '+ goal
+    // if (goal >= 80){document.getElementById('screen').innerHTML='you passed bitch'}
     if(cor1>100 || cor2>100 || cor3>100 || cor4>100 || cor5>100 || cor6>100 || cor7>100) alert('modafucker go and reduce the score value below 100');
+    // if (add >80 ){
+    //     document.getElementById('bigscreen').innerHTML= add + 'you passed bitch'
+    // }
 }
